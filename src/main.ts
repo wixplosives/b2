78dd62dc2cfe7e02b48ea49e08ffa1a858ca654d
alexshe <alexshe@wix.com>
2020-08-11 12:48:40 +0300

    fix: more power to tokens

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
   try {
     const commentText: string = core.getInput('commentText')
     if (commentText.includes('@measure')) {
-      core.debug(`Found measure command`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+      core.info(`Found measure command`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
       const octokit = new Octokit()
       await octokit.request(
         'POST repos/wixplosives/test-p-1/actions/workflows/2144869/dispatches',
