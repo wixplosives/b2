f7de1bad62edc07d732ac78ca8aa11be59353dd2
alexshe <alexshe@wix.com>
2020-08-11 12:38:38 +0300

    fix: octokit

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ async function run(): Promise<void> {
     if (commentText.includes('@measure')) {
       core.debug(`Found measure command`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
       const octokit = new Octokit()
-      await octokit.request( 'POST repos/wixplosives/test-p-1/actions/workflows/2144869/dispatches',
+      await octokit.request(
+        'POST repos/wixplosives/test-p-1/actions/workflows/2144869/dispatches',
         {
-          owner: 'wixplosives',
-          repo: 'test-p-1',
-          workflow_id: 'test.yaml',
           ref: 'master'
         }
       )
